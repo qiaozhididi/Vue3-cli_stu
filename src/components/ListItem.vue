39d273d1025baabaa7874a4cd716665003b5d635
qiaozhididi <1343013603@qq.com>
2023-04-11 22:17:59 +0800

    20230411

---
+++ src/components/ListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <li>{{ msg }}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
