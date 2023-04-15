778ee467f1c4531cc23e089688f564fbf463d75c
qiaozhididi <1343013603@qq.com>
2023-04-15 22:59:23 +0800

    20230415---mutation同步异步

--- src/store/index.js
+++ src/store/index.js
@@ -5,21 +5,23 @@ export default createStore({
   state: {
     name: "QzFrato",
   },
+  //commit和mutations做关联
   mutations: {
     // mutations里面只允许写同步的代码，不允许写异步的代码
     //第四步 对应的mutation被执行
-    setName(store,str) {
+    setName(state,str) {
       //第五步 在mutation中修改state中的数据
       // console.log("mutations");
-      this.state.name = str;
+      state.name = str;
     },
   },
+  //dispatch和actions做关联
   actions: {
     //第二步，store感知到你触发了一个叫做setName的action，然后执行setName方法
     setName(store,str) {
       //第三步，提交一个commit触发一个mutation
       setTimeout(() => {
-        this.commit("setName", str);
+        store.commit("setName", str);
       }, 2000);
     },
   },
